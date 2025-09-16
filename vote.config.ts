ed2bdb681a7bab8f0b51a49963b20198e1b498ff
JaySayed <jay.s.sayed@gmail.com>
2025-09-16 15:27:48 -0400

    Add Vite configuration for React project

---
+++ vote.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './client/src'),
+      '@assets': path.resolve(__dirname, './attached_assets'),
+    }
+  },
+  build: {
+    outDir: 'dist/public',
+    rollupOptions: {
+      input: './client/index.html'
+    }
+  },
+  root: './client',
+  server: {
+    port: 5173,
+    host: '0.0.0.0'
+  }
+})
